0d59d80508b12bd70799981bda2fd4a7524a251c
Shawn Junell <Shawn.Junell@pemco.com>
2018-06-04 15:06:19 -0700

    update jenkins

--- Jenkins/create_sslcert.sh
+++ Jenkins/create_sslcert.sh
@@ -4,9 +4,9 @@ cd ~/ssl
 
 # Create SSL Cert
 openssl req \
-       -newkey rsa:2048 -nodes -keyout domain.key \
+       -newkey rsa:4096 -nodes -keyout domain.key \
        -subj "/C=US/ST=Washginton/L=Seattle/O=IT/CN=jenkins" \
-       -x509 -days 365 -out domain.crt
+       -x509 -sha256 -days 365 -out domain.pem
 
 cp domain.key /etc/ssl/domain.key
-cp domain.crt /etc/ssl/domain.crt
+cp domain.crt /etc/ssl/domain.pem
